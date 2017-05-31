64d07e2241e442a3de911a79ea6f180a67a39233
Piotr Brudny <pbrudny@gmail.com>
2017-05-31 23:31:51 +0200

    Remove pry usage

--- app/mailers/ad_mailer.rb
+++ app/mailers/ad_mailer.rb
@@ -1,6 +1,5 @@
 class AdMailer < ApplicationMailer
   def new_ads(ads)
-    binding.pry
     @user = User.first
     @ads  = ads
     mail(to: 'pbrudny@gmail.com', subject: 'New apartments for you!')
