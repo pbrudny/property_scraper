d1ad75f11044456f9ab699e25f867450b9fc664d
Piotr Brudny <pbrudny@gmail.com>
2017-06-04 19:21:36 +0200

    Do not notify about 0 ads

--- app/services/ads/load_ads.rb
+++ app/services/ads/load_ads.rb
@@ -8,7 +8,7 @@ module Ads
 
     def call
       new_ads = search_links.map { |link| [link.name, scraped_new_ads(link) ] }
-      notify_about_new(new_ads)
+      notify_about_new(new_ads) if new_ads?(new_ads)
     end
 
     private
@@ -21,6 +21,14 @@ module Ads
 
     def scraped_new_ads(search_link)
       Scrapers::Olx::ScrapeAll.new(search_link).call
+    end
+
+    def new_ads?(new_ads)
+      ads_count(new_ads) > 0
+    end
+
+    def ads_count(ads)
+      ads.flatten.count - ads.count
     end
   end
 end
