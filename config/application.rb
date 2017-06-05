528081572405dfd2a833a87bf97ccb32873ba7d8
Piotr Brudny <pbrudny@gmail.com>
2017-06-05 17:59:23 +0200

    Add polish time zone

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module PropertyScraper
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths << Rails.root.join('lib')
+    config.time_zone = 'Warsaw'
   end
 end
