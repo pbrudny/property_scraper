80cbc7f24cab67614910b8f10d794f76ed063af7
Piotr Brudny <pbrudny@gmail.com>
2017-06-06 15:54:48 +0200

    Do not display search link name when no ads

--- spec/mailers/previews/ad_mailer_preview.rb
+++ spec/mailers/previews/ad_mailer_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/ad_mailer
 class AdMailerPreview < ActionMailer::Preview
   def new_ads
-    AdMailer.new_ads([[SearchLink.first.name, Ad.all[0..3]], [SearchLink.second.name, Ad.all[4..6]]])
+    AdMailer.new_ads([[SearchLink.first.name, Ad.all[0..3]], [SearchLink.second.name, Ad.all[4..6]], [SearchLink.second.name, []]])
   end
 end
