636b8840ed2273bc00f1bd524b0fc479d53075cc
pbrudny <pbrudny@gmail.com>
2018-04-23 21:53:16 +0200

    add domain ENV

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
 
-  config.action_mailer.default_url_options = { :host => 'property-scraper-pbrudny.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => ENV['DOMAIN'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: 'property-scraper-pbrudny.herokuapp.com',
+    domain: ENV['DOMAIN'],
     authentication: 'plain',
     enable_starttls_auto: true,
     user_name: ENV['GMAIL_USERNAME'],
